11afdf97b6f8f6ff1823e10d6610495e6e14c7a6
Dainis Dilevka <mozart_dd@outlook.com>
2025-03-13 13:29:44 +0000

    created equal div blocks which will become sketch board

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const container = document.querySelector('.container');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
-let sizeOfGrid = 3;
+let sizeOfGrid = 6;
 
 // button.addEventListener('click', () => {
 //     sizeOfGrid = input.value;
 // });
-
-
 
 //LOOP TO CREATE DIV SQUARES EQUAL TO sizeOfGrid VARIABLE COUNT
 for (let i = 1; i <= sizeOfGrid * sizeOfGrid; i++) {
@@ -16,14 +14,15 @@ for (let i = 1; i <= sizeOfGrid * sizeOfGrid; i++) {
     container.appendChild(div);
 
 //DIV STYLING PART
-    div.style.flex = '1 1 0'
-    div.style.border = 'solid, 1px, black'
-    div.style.backgroundColor = 'white'
+    // div.style.flex = 'auto';
+    div.style.border = 'solid, 1px, black';
+    div.style.backgroundColor = 'white';
+    div.style.width = `${100 / sizeOfGrid}%`;
+    div.style.height = `${100 / sizeOfGrid}%`;
 
 //EVENT LISTENER TO CHANGE COLOR IF MOUSE IS OVER DIV
-    div.addEventListener('mouseover' , () => {
+    div.addEventListener('mouseenter' , () => {
         div.style.backgroundColor = ''
         div.classList.add('on-hover');
     })
-
 };
