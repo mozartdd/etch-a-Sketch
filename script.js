e942fcc0de35ce0b97351681ffce696b0b6288e4
Dainis Dilevka <mozart_dd@outlook.com>
2025-03-15 09:54:31 +0000

    PROJECT OF ETCH-E-SKETCH

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const button = document.querySelector('button');
 
 //FUNCTION WHICH CHANGES THE AMOUNT OF DIV SQUARES
 function resizeGrid(sizeOfGrid){
+
 //CLEARS CONTAINER OF PREVIOUS DIV IF NEW DIV AMOUNT IS ENTERED
     container.innerHTML = '';
 //LOOP TO CREATE DIV SQUARES EQUAL TO sizeOfGrid VARIABLE COUNT
@@ -17,24 +18,27 @@ function resizeGrid(sizeOfGrid){
         div.style.width = `${100 / sizeOfGrid}%`;
         div.style.height = `${100 / sizeOfGrid}%`;
     
-    //EVENT LISTENER TO CHANGE COLOR IF MOUSE IS OVER DIV
-        div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = '';
-            div.classList.add('on-hover');
-        });
-    
-    };  
+    //EVENT LISTENER TO CHANGE COLOR IF MOUSE IS OVER DIV     
+    div.addEventListener('mouseenter', () => {
+        // div.style.backgroundColor = '';
+        div.style.backgroundColor = `rgb(${returnRandomRGB()}, ${returnRandomRGB()}, ${returnRandomRGB()})`});
+        
 }
+}
+
+//FUNCTION THAT RETURNS RANDOM NUM FOR RGB COLOR 
+function returnRandomRGB() {
+    return Math.floor((Math.random() * 255) + 1)
+};
+
 
 //FUNCTION TO ASK FOR USER INPUT TO CHOSE SQUARE AMOUNT
 function askForSize() {
     if(!input.value) {
         input.value = 16;
-    }
-    if(input.value > 100 ) {
+    } else if(input.value > 100 ) {
         input.value = 100;
-    }
-    if(input.value <= 0) {
+    } else if(input.value <= 0) {
         input.value = 1;
     }
     return input.value;
@@ -47,61 +51,4 @@ resizeGrid(16);
 //BUTTON EVENT LISTENER TO TRIGGER PROMPT TO ASK FOR USER INPUT
 button.addEventListener('click', () => {
     resizeGrid(askForSize());
-})
-
-
-//COLOR CHANGERS MAY BE WILL NOT USE 
-
-// const red = document.querySelector('.red');
-// const blue = document.querySelector('.blue');
-// const green = document.querySelector('.green');
-// const pink = document.querySelector('.pink');
-// const yellow = document.querySelector('.yellow');
-// const purple = document.querySelector('.purple');
-// const black = document.querySelector('.black');
-// const white = document.querySelector('.white');
-
-// red.addEventListener('click', () => {
-    //     div.classList.remove('on-hover-blue');
-    //     div.classList.remove('on-hover-green');
-    //     div.classList.remove('on-hover-pink');
-    //     div.classList.remove('on-hover-yellow');
-    //     div.classList.remove('on-hover-purple');
-    //     div.classList.remove('on-hover-black');
-    //     div.classList.add('on-hover-red')
-    // });
-
-    // white.addEventListener('click', () => {
-    //     // div.classList.remove();
-    //     div.classList.add('on-hover-white')
-    // });
-
-    // blue.addEventListener('click', () => {
-    //     // div.classList.remove();
-    //     div.classList.add('on-hover-blue')
-    // });
-
-    // green.addEventListener('click', () => {
-    //     // div.classList.remove('on-hover');
-    //     div.classList.add('on-hover-green')
-    // });
-
-    // pink.addEventListener('click', () => {
-    //     // div.classList.remove('on-hover');
-    //     div.classList.add('on-hover-pink')
-    // });
-
-    // yellow.addEventListener('click', () => {
-    //     // div.classList.remove('on-hover');
-    //     div.classList.add('on-hover-yellow')
-    // });
-
-    // purple.addEventListener('click', () => {
-    //     div.classList.remove('on-hover');
-    //     div.classList.add('on-hover-purple')
-    // });
-
-    // black.addEventListener('click', () => {
-    //     // div.classList.remove('on-hover');
-    //     div.classList.add('on-hover-black')
-    // });
+});
